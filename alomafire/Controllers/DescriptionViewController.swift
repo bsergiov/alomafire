278379b274cb0c2c2936f7a37805c4a10342743c
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-01-20 14:04:07 +0200

    added parse json on alamofire

--- alomafire/Controllers/DescriptionViewController.swift
+++ alomafire/Controllers/DescriptionViewController.swift
@@ -10,9 +10,64 @@ import Alamofire
 
 class DescriptionViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var signImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var luckyNumber: UILabel!
+    @IBOutlet weak var luckyTime: UILabel!
+    
+    @IBOutlet weak var paraetrsStackView: UIStackView!
+    
+    // MARK: - Public Properties
+    var sign: String!
+    
+    // MARK: - Private Properties
+    var indicator = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        showIndicator()
+        setupFirstContent()
+        fetchData()
+        
+    }
+}
+
+// MARK: - Private Methodes
+extension DescriptionViewController {
+    private func setupFirstContent() {
+        title = sign
+        signImage.image = UIImage(named: sign)
+    }
+    
+    private func showIndicator() {
+        indicator.center = view.center
+        indicator.hidesWhenStopped = true
+        view.addSubview(indicator)
+        indicator.startAnimating()
+    }
+    
+    private func fetchData() {
+        NetworkManager.shared.fetchForacast(for: sign) { forecast in
+            guard
+                let description = forecast.description,
+                let date = forecast.currentDate,
+                let color = forecast.color,
+                let luckyNumber = forecast.luckyNumber,
+                let luckyTime = forecast.luckyTime
+            else { return }
+            self.descriptionLabel.text = description
+            self.dateLabel.text = "Today \(date)"
+            self.colorLabel.text = "You color: \(color)"
+            self.luckyNumber.text = "You lucky number: \(luckyNumber)"
+            self.luckyTime.text = "You lucky time: \(luckyTime)"
+            self.indicator.stopAnimating()
+            self.paraetrsStackView.isHidden = false
+            self.descriptionLabel.isHidden = false
+        }
     }
 }
--- alomafire/Services/NetworkManager.swift
+++ alomafire/Services/NetworkManager.swift
@@ -6,7 +6,64 @@
 //
 
 import Foundation
+import Alamofire
+
+enum NetworkError: Error {
+    case invalidUrl
+    case noData
+    case decodingError
+}
+
+// MARK: - Signs
+enum Signs: String, CaseIterable {
+    
+    case aries = "Aries"
+    case taurus = "Taurus"
+    case gemini = "Gemini"
+    case cancer = "Cancer"
+    case leo = "Leo"
+    case virgo = "Virgo"
+    case libra = "Libra"
+    case scorpio = "Scorpio"
+    case sagittarius = "Sagittarius"
+    case capricorn = "Capricorn"
+    case aquarius = "Aquarius"
+    case pisces = "Pisces"
+}
 
 class NetworkManager {
     
+    static let shared = NetworkManager()
+    
+    private init() {}
+ 
+    func fetchForacast(for sign: String, completionHandler: @escaping(Forecast) -> Void) {
+        let urlString = "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=\(sign)&day=Today"
+        let headers = HTTPHeaders(
+            ["x-rapidapi-key" : "8cfd233fd1mshbc3fd7aed9f5f7bp1311d6jsn965cb2614615"]
+        )
+        AF.request(urlString, method: .post, headers: headers)
+            .validate()
+        
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+              
+                case .success(let value):
+                    guard let forecastData = value as? [String: Any] else { return }
+                    let forecast = Forecast(
+                        dateRange: forecastData["date_range"] as? String,
+                        currentDate: forecastData["current_date"] as? String,
+                        description: forecastData["description"] as? String,
+                        compatibility: forecastData["compatibility"] as? String,
+                        mood: forecastData["mood"] as? String,
+                        color: forecastData["color"] as? String,
+                        luckyNumber: forecastData["lucky_number"] as? String,
+                        luckyTime: forecastData["lucky_time"] as? String
+                    )
+                    completionHandler(forecast)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
 }
